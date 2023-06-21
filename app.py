bfd6edc4da212ae1722c67e8259f2ce264b9c933
spc2901 <96281229+spc2901@users.noreply.github.com>
2023-06-21 10:28:20 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import requests
 def fetch_poster(movie_id):
     response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=b715bc783321b6be1a04dd8658ceae69&&language=en-US'.format(movie_id))
     data = response.json()
-    return "https://image.tmdb.org/t/p/original/" + data['poster_path']
+    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
 
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
@@ -38,7 +38,7 @@ selected_movie_name = st.selectbox('Type or select a movie from the dropdown', m
 if st.button('Recommend Movies'):
     names, posters = recommend(selected_movie_name)
 
-    col1, col2, col3, col4, col5 = st.beta_columns(5)
+    col1, col2, col3, col4, col5 = st.columns(5)
 
     with col1:
         st.text(names[0])
